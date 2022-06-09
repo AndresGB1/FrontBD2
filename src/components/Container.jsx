18d8fa44f2b2d6f7dfc35d27f0ed4d2887c6a90d
AndresGB1 <a.galvisbolivar@gmail.com>
2022-06-09 18:01:01 -0500

    search

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,14 +3,16 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
+import Button from "@mui/material/Button";
 
 export default function Container(props) {
   const [productos, setProductos] = useState([]);
   const [categorias, setCategorias] = useState([]);
+  const [buscas, setBuscas] = useState([]);
   const getCategoria = () => {
     axios
       .get(process.env.REACT_APP_ENDPOINT + `/categorias`)
-      .then((res) => {       
+      .then((res) => {
         setCategorias(res.data.toString().split(","));
       })
       .catch((err) => {
@@ -22,12 +24,13 @@ export default function Container(props) {
       .get(process.env.REACT_APP_ENDPOINT + `/productos`)
       .then((res) => {
         setProductos(res.data);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
   };
-  const getProductoByCategoria = (categoria) => { 
+  const getProductoByCategoria = (categoria) => {
     axios
       .get(process.env.REACT_APP_ENDPOINT + `/productos/` + categoria)
       .then((res) => {
@@ -37,6 +40,17 @@ export default function Container(props) {
         console.log(err);
       });
   };
+
+  const buscarProducto = () => {
+    setProductos([]);
+    productos.map((producto) => {
+      if (producto[1].toLowerCase().startsWith(buscas.toLowerCase())) {
+        setProductos([...productos, producto]);
+
+      }
+    });
+  };
+
   useEffect(() => {
     getProductos();
     getCategoria();
@@ -44,26 +58,44 @@ export default function Container(props) {
 
   return (
     <div style={{ padding: "5%" }}>
-      <h2 style={{color: "black"}}>Products </h2>
-    <div style={{textAlign:"center", display:"flex", justifyContent:"space-around"}}>
-      <Autocomplete
-        disablePortal
-        id="combo-box-demo"
-        options={categorias}
-        sx={{ width: 300,paddingBottom: "5%" , color: "white" }}
-        onChange={(event, e) => {getProductoByCategoria(e)}}
-        renderInput={(params) => <TextField {...params} label="Categoria" />}
-      />
-      
-       <TextField 
-       id="standard-basic" 
-       label="¿Que buscas? " 
-       variant="standard" 
-       sx={{ width: 300, color: "white"}}
-        
-       />
-       </div>
-    
+      <h2 style={{ color: "black" }}>Products </h2>
+      <div
+        style={{
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "space-around",
+        }}
+      >
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          options={categorias}
+          sx={{ width: 300, paddingBottom: "5%", color: "white" }}
+          onChange={(event, e) => {
+            getProductoByCategoria(e);
+          }}
+          renderInput={(params) => <TextField {...params} label="Categoria" />}
+        />
+
+        <TextField
+          id="standard-basic"
+          label="¿Que buscas? "
+          variant="standard"
+          sx={{ width: 300, color: "white" }}
+          value={buscas}
+          onChange={(event) => {
+            setBuscas(event.target.value);
+          }}
+        />
+      </div>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{ marginBottom: "5%" }}
+        onClick={buscarProducto}
+      >
+        Buscar
+      </Button>
       {productos.map((producto, key) => (
         <div key={key} style={{ paddingBottom: "3%" }}>
           <Card
